cc4f66c3bb408bfe0f70c54a407babdabc2448e7
Debanjum Singh Solanky <debanjum@gmail.com>
2022-04-15 19:05:32 -0400

    Use css grid width to control line wrap on home page
    Instead of inserting explicit line breaks, which breaks clean
    responsive UI rendering on differently sized screens like phones.

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -4,37 +4,51 @@
     This is the home page for the application. It doesn't really do much, but it's a good place to start.
  -->
 
-<p class="description">
-    <b class="section-title">Summary</b><br />
-    <i>Increase Exploration, Reduce Decisions 🚶🏽‍♀</i><br />
+<div class="content">
+    <div class="section">
+    <div class="section-title">Microdosing on Randomness</div>
+        <div class="section-subtitle">Increase Exploration, Reduce Decisions 🚶‍♀️</div>
+        <p>
+            Welcome Meandering Fellow!
+            Use this site to explore your <a href=/map>neighborhood</a> <a href=/map/restaurant>restaurants</a>, <a href="/map/bar">bars</a>, <a href="/map/coffee">coffee shops</a>, <a href="/map/park">parks</a> or <a href="/map/bookstore">bookstores</a>.
+        </p>
+    </div>
 
-    Welcome to Meandering Fellow! Use it to explore your <a href=/map>neighborhood</a> <a href=/map/restaurant>restaurants</a>, <a href="/map/bar">bars</a>, <a href="/map/coffee">coffee shops</a>, <a href="/map/park">parks</a> or <a href="/map/bookstore">bookstores</a>.<br />
-</p>
+    <div class="section">
+        <div class="section-title">Why should I randomize?</div>
+        <p>Leave some decisions to randomness. It will encourage exploration and reduce decision fatigue.</p>
 
-<p class="description">
-    <b class="section-title">Details</b><br />
-    Leave some decisions to randomness to encourage exploration and reduce decision fatigue.
+        <ul>
+            <li>
+                If you want to explore a city, randomize the next bookstore or park you visit.
+                You'll get to see those nooks and crannies of the city you wouldn't ordinarily visit
+            </li>
 
-    <ul>
-        <li>
-            If you want to explore a city, randomize the next bookstore or park you visit.<br />
-            You'll get to see those nooks and crannies of the city you wouldn't ordinarily visit.<br /><br />
-        </li>
-
-        <li>
-            If you want to reduce decision fatigue, and take a break from an ever-increasing set of choices.<br />
-            Tradeoff optimization for exploration and reduced decisions.<br />
-            Randomize the next restaurant, bar or coffee shop you visit.<br />
-        </li>
-    </ul>
+            <li>
+                If you want to reduce decision fatigue, and take a break from an ever-increasing set of choices.
+                Tradeoff optimization for exploration and reduced decisions.
+                Randomize the next restaurant, bar or coffee shop you visit
+            </li>
+        </ul>
+    </div>
+</div>
 
 <style>
-    .description {
-        font-size: 16px;
-        padding: 12px;
+    .content {
+        display: grid;
+        grid-template-columns: min(60ch, calc(100% - 16px));
+        max-width: 1800px;
+        font-size: 20px;
     }
-
+    .section {
+        padding: 16px 32px 0px;
+        line-height: 1.5;
+    }
     .section-title {
         font-size: 24px;
+    }
+    .section-subtitle {
+        font-size: 16px;
+        font-style: italic;
     }
 </style>
